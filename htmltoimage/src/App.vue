d789f2b0673da1f389636d5a4db4f6c7fab13bf9
Michał Białkowski <michal@bialkowski.net.pl>
2022-11-28 18:26:43 +0100

    Update

---
+++ htmltoimage/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+	import domtoimage from 'dom-to-image';
+	
+	export default {
+		name: 'App',
+		components: {
+		},
+		setup() {
+			var node = document.getElementById('print');
+			
+			domtoimage.toPng(node)
+			.then(function (dataUrl) {
+				var img = new Image();
+				img.src = dataUrl;
+				document.body.appendChild(img);
+			})
+			.catch(function (error) {
+				console.error('oops, something went wrong!', error);
+			});
+		}
+	}
+</script>
+
+<style>
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
